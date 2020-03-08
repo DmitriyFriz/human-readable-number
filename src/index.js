077ac9282505768504646d761449dfe06e3d9d6e
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-03-08 21:37:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function toReadable (number) {
-  
+    let objNumbers = {
+        0:'zero', 
+        1:'one', 
+        2:'two', 
+        3:'three', 
+        4:'four', 
+        5:'five', 
+        6:'six', 
+        7:'seven', 
+        8:'eight', 
+        9:'nine',
+        10:'ten', 
+        11:'eleven', 
+        12:'twelve', 
+        13:'thirteen', 
+        14:'fourteen', 
+        15:'fifteen', 
+        16:'sixteen', 
+        17:'seventeen', 
+        18:'eighteen', 
+        19:'nineteen', 
+        20:'twenty',
+        30:'thirty',
+        40:'forty',
+        50:'fifty',
+        60:'sixty',
+        70:'seventy',
+        80:'eighty',
+        90:'ninety',
+    }
+
+    function calcDecimalPlaces(number) {
+        if (number < 20) return objNumbers[number];
+        if (number < 100) {
+            let result = objNumbers[Math.trunc(number / 10) * 10];
+            if (number % 10) result = result + ' ' + objNumbers[number % 10];
+            return result;
+        }  
+    }
+    
+    if (100 <= number && number < 1000) {
+        if (! (number % 100)) return objNumbers[Math.trunc(number / 100)] + ' hundred';
+        return objNumbers[Math.trunc(number / 100)] + ' hundred' + ' ' +  calcDecimalPlaces(number % 100);
+    }
+
+    return calcDecimalPlaces(number); 
 }
